755ca6ea860d68f25be7e764f8df966587dff378
ruth.mesfun <ruth.mesfun@flatironschool.com>
2015-07-28 05:49:27 -0400

    All Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = integer
+  while counter > 0
+    puts"#{counter} SECOND(S)!"
+    counter = counter - 1
+  end
+    "HAPPY NEW YEAR!" 
+end
+
+def countdown_with_sleep(num)
+  sleep(num)
 end
